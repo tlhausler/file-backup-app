a908e21526ce6528441db618c752fb2f2ed67832
Tamber Hausler <tamber.hausler@mail.helenacollege.edu>
2022-05-01 23:22:22 -0600

    added comments to code

--- BackerUpper/Form1.cs
+++ BackerUpper/Form1.cs
@@ -13,31 +13,37 @@ namespace BackerUpper
 {
     public partial class Form1 : Form
     {
+        // instantiate backgroundworker that will handle file operations
         BackgroundWorker worker = new BackgroundWorker();
         public Form1()
         {
+            // initialize component and set its DoWork starting point to the Worker_DoWork method
             InitializeComponent();
             worker.DoWork += Worker_DoWork;
         }
 
         void BackupFile(string source, string destination)
         {
+            // open file stream for both input and output.
             FileStream fsIn = new FileStream(source, FileMode.Open);
             FileStream fsOut = new FileStream(destination, FileMode.Create);
             byte[] b = new byte[1048756];
             int readByte;
 
+            // instruct filestream output to write information from the input
             while ((readByte = fsIn.Read(b, 0, b.Length)) > 0)
             {
                 fsOut.Write(b, 0, readByte);
             }
 
+            // close filestream operations
             fsIn.Close();
             fsOut.Close();
         }
 
         private void Worker_DoWork(object sender, DoWorkEventArgs e)
         {
+            // call the BackupFile method and pass it the source and destination paths
             BackupFile(txtSourceFile.Text, txtBackupDir.Text);
         }
 
@@ -48,6 +54,7 @@ namespace BackerUpper
 
         private void btnSourceBrowse_Click(object sender, EventArgs e)
         {
+            // instantiate a file dialog box for the browse button to choose file to backup
             OpenFileDialog o = new OpenFileDialog();
             if(o.ShowDialog() == DialogResult.OK)
             {
@@ -57,6 +64,7 @@ namespace BackerUpper
 
         private void btnBackupBrowse_Click(object sender, EventArgs e)
         {
+            // instantiate folder browser dialog box to select folder location to save selected file to
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             if(fbd.ShowDialog() == DialogResult.OK)
             {
@@ -66,6 +74,7 @@ namespace BackerUpper
 
         private void btnBackup_Click(object sender, EventArgs e)
         {
+            // have the backgroundworker perform the copy
             worker.RunWorkerAsync();
         }
     }
